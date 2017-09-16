28e65013d61a46cebda38598fc0588c5b1f9d505
Parit Bansal <parit.bansal@isb-sib.ch>
2017-09-16 15:00:25 +0200

    base dialogs

--- index.js
+++ index.js
@@ -1,137 +1,77 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a sample skill built with Amazon Alexa Skills nodejs
- * skill development kit.
- * This sample supports multiple languages (en-US, en-GB, de-GB).
- * The Intent Schema, Custom Slot and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-howto
- **/
-
-'use strict';
-
-const Alexa = require('alexa-sdk');
-const recipes = require('./recipes');
-
-const APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
-
-const languageStrings = {
-    'en': {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'Minecraft Helper',
-            WELCOME_MESSAGE: "Hi i am smarty i help you.",
-            WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-            DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
-            STOP_MESSAGE: 'Goodbye!',
-            RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
-            RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
-        },
-    },
-    'en-US': {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'American Minecraft Helper',
-        },
-    },
-    'en-GB': {
-        translation: {
-            RECIPES: recipes.RECIPE_EN_GB,
-            SKILL_NAME: 'British Minecraft Helper',
-        },
-    },
-    'de': {
-        translation: {
-            RECIPES: recipes.RECIPE_DE_DE,
-            SKILL_NAME: 'Assistent für Minecraft in Deutsch',
-            WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?',
-            WELCOME_REPROMPT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
-            DISPLAY_CARD_TITLE: '%s - Rezept für %s.',
-            HELP_MESSAGE: 'Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-            RECIPE_REPEAT_MESSAGE: 'Sage einfach „Wiederholen“.',
-            RECIPE_NOT_FOUND_MESSAGE: 'Tut mir leid, ich kenne derzeit ',
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'das Rezept für %s nicht. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'dieses Rezept nicht. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'Womit kann ich dir sonst helfen?',
-        },
-    },
-};
-
-const handlers = {
+var Alexa = require('alexa-sdk');
+const states = [
+  "NONE",
+  "CONFIRM_REQUEST_MEDICINE",
+  "CONFIRM_MEDICINE",
+  "CONFIRM_PAYMENT_METHOD"
+];
+var currentState = "NONE";
+var recheckMsg = "I am going to check on you again in 2 hours. If you don't feel better, I will call your husband or the doctor for you.";
+var handlers = {
     'LaunchRequest': function () {
-        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
-        this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+      currentState = "NONE";
+      this.emit(":ask", "Dora, how are you and how was the tea party last night?");
+      console.log("current state is " + currentState);
     },
-    'RecipeIntent': function () {
-        const itemSlot = this.event.request.intent.slots.Item;
-        let itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
-
-        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
-        const myRecipes = this.t('RECIPES');
-        const recipe = myRecipes[itemName];
-
-        if (recipe) {
-            this.attributes.speechOutput = recipe;
-            this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
-            this.emit(':askWithCard', recipe, this.attributes.repromptSpeech, cardTitle, recipe);
-        } else {
-            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
-            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
-            if (itemName) {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
-            } else {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
-            }
-            speechOutput += repromptSpeech;
-
-            this.attributes.speechOutput = speechOutput;
-            this.attributes.repromptSpeech = repromptSpeech;
-
-            this.emit(':ask', speechOutput, repromptSpeech);
-        }
+    'AMAZON.YesIntent': function() {
+      switch(currentState) {
+        case "CONFIRM_REQUEST_MEDICINE" :
+          this.emit("ConfirmMedicineIntent");
+          break;
+        case "CONFIRM_MEDICINE":
+          this.emit('PaymentOptionIntent');
+          break;
+        case "CONFIRM_PAYMENT_METHOD":
+          this.emit("ConfirmAccountAmount");
+          break;
+        default:
+          break;
+      }
     },
-    'AMAZON.HelpIntent': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
-        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    'AMAZON.NoIntent': function() {
+      switch(currentState) {
+        case "CONFIRM_REQUEST_MEDICINE" :
+          currentState = "NONE";
+          this.emit(":tell", "Well.. hope you get better soon! " + recheckMsg);
+          break;
+        case "CONFIRM_MEDICINE":
+          currentState = "NONE";
+          this.emit(":tell", "Fine. Take some rest." + recheckMsg);
+          break;
+        case "CONFIRM_PAYMENT_METHOD":
+          currentState = "NONE";
+          this.emit(":tell", "Fine. then keep 50 Francs handy. Your medicine will be delivered to your door in about 20 minutes." + recheckMsg);
+          break;
+        default:
+          break;
+      }
     },
-    'AMAZON.RepeatIntent': function () {
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
+    'OrderAspirinIntent': function() {
+      currentState = "CONFIRM_REQUEST_MEDICINE";
+      this.emit(":ask", "I have already ordered it twice this week. Are you sure that you want to order more and remove"
+      + " rum from your shopping list?");
+      console.log("current state is " + currentState);
     },
-    'AMAZON.StopIntent': function () {
-        this.emit('SessionEndedRequest');
+    'ConfirmMedicineIntent': function() {
+      currentState = "CONFIRM_MEDICINE";
+      this.emit(":ask", "Let me check which pharmarcy can deliver it today. By the way, my name is Smarty, not smarty-pants. May I suggest ibuprofen?");
+      console.log("current state is " + currentState);
     },
-    'AMAZON.CancelIntent': function () {
-        this.emit('SessionEndedRequest');
+    'PaymentOptionIntent': function() {
+      currentState = "CONFIRM_PAYMENT_METHOD";
+      this.emit(":ask", "I have ordered it, would you like to pay in advance using your bank account ?");
+      console.log("current state is " + currentState);
     },
-    'SessionEndedRequest': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
-    'Unhandled': function () {
-        this.attributes.speechOutput = this.t('HELP_MESSAGE');
-        this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-        this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
-    },
+    'ConfirmAccountAmount': function() {
+        currentState = "NONE";
+        this.emit(":tell", "Okay, the  current account balance is 1500 CHF after this payment. Your medicine will be delivered to your door in about 20 minutes." + recheckMsg);
+        console.log("current state is " + currentState);
+    }
 };
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context, callback);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
