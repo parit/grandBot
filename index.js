95a11c8afb6ae00986df1f3bf221f415c9c0f2c0
Parit Bansal <parit.bansal@isb-sib.ch>
2017-09-16 16:44:56 +0200

    working

--- index.js
+++ index.js
@@ -1,77 +1,99 @@
 var Alexa = require('alexa-sdk');
 const states = [
-  "NONE",
-  "CONFIRM_REQUEST_MEDICINE",
-  "CONFIRM_MEDICINE",
-  "CONFIRM_PAYMENT_METHOD"
+  "_NONE",
+  "_CONFIRM_REQUEST_MEDICINE",
+  "_CONFIRM_MEDICINE",
+  "_CONFIRM_PAYMENT_METHOD"
 ];
-var currentState = "NONE";
 var recheckMsg = "I am going to check on you again in 2 hours. If you don't feel better, I will call your husband or the doctor for you.";
-var handlers = {
-    'LaunchRequest': function () {
-      currentState = "NONE";
-      this.emit(":ask", "Dora, how are you and how was the tea party last night?");
-      console.log("current state is " + currentState);
-    },
+
+var confirmRequestMedicineHandlers = Alexa.CreateStateHandler("_CONFIRM_REQUEST_MEDICINE", {
     'AMAZON.YesIntent': function() {
-      switch(currentState) {
-        case "CONFIRM_REQUEST_MEDICINE" :
-          this.emit("ConfirmMedicineIntent");
-          break;
-        case "CONFIRM_MEDICINE":
-          this.emit('PaymentOptionIntent');
-          break;
-        case "CONFIRM_PAYMENT_METHOD":
-          this.emit("ConfirmAccountAmount");
-          break;
-        default:
-          break;
-      }
-    },
-    'AMAZON.NoIntent': function() {
-      switch(currentState) {
-        case "CONFIRM_REQUEST_MEDICINE" :
-          currentState = "NONE";
-          this.emit(":tell", "Well.. hope you get better soon! " + recheckMsg);
-          break;
-        case "CONFIRM_MEDICINE":
-          currentState = "NONE";
-          this.emit(":tell", "Fine. Take some rest." + recheckMsg);
-          break;
-        case "CONFIRM_PAYMENT_METHOD":
-          currentState = "NONE";
-          this.emit(":tell", "Fine. then keep 50 Francs handy. Your medicine will be delivered to your door in about 20 minutes." + recheckMsg);
-          break;
-        default:
-          break;
-      }
-    },
-    'OrderAspirinIntent': function() {
-      currentState = "CONFIRM_REQUEST_MEDICINE";
-      this.emit(":ask", "I have already ordered it twice this week. Are you sure that you want to order more and remove"
-      + " rum from your shopping list?");
-      console.log("current state is " + currentState);
+        console.log("Got into yes and no intent");
+        this.emitWithState("ConfirmMedicineIntent");
     },
     'ConfirmMedicineIntent': function() {
-      currentState = "CONFIRM_MEDICINE";
-      this.emit(":ask", "Let me check which pharmarcy can deliver it today. By the way, my name is Smarty, not smarty-pants. May I suggest ibuprofen?");
-      console.log("current state is " + currentState);
+      this.handler.state = "_CONFIRM_MEDICINE";
+      console.log("current state is " + this.handler.state);
+      this.emit(":ask", "Let me check which pharmacy can deliver it today. By the way, my name is Smarty, not smarty-pants. May I suggest ibuprofen?");
+    },
+    'AMAZON.NoIntent': function() {
+      this.handler.state = "NONE";
+      this.emit(":tell", "Well.. hope you get better soon! " + recheckMsg);
+    },
+    'Unhandled': function() {
+      console.log("Can't understand user input " + this.handler.state);
+      this.emit(":ask", "Sorry Doora, I didn't understand you could you say that again");
+    }
+});
+
+var confirmMedicineHandlers = Alexa.CreateStateHandler("_CONFIRM_MEDICINE", {
+    'AMAZON.YesIntent': function() {
+        this.emitWithState("PaymentOptionIntent");
     },
     'PaymentOptionIntent': function() {
-      currentState = "CONFIRM_PAYMENT_METHOD";
+      this.handler.state = "_CONFIRM_PAYMENT_METHOD";
+      console.log("current state is " + this.handler.state);
       this.emit(":ask", "I have ordered it, would you like to pay in advance using your bank account ?");
-      console.log("current state is " + currentState);
+    },
+    'AMAZON.NoIntent': function() {
+      this.handler.state = "NONE";
+      this.emit(":tell", "Fine. Take some rest." + recheckMsg);
+    },
+    'Unhandled': function() {
+      console.log("Can't understand user input " + this.handler.state);
+      this.emit(":ask", "Sorry Doora, I didn't understand you could you say that again");
+    }
+});
+
+
+var confirmPaymentMethods = Alexa.CreateStateHandler("_CONFIRM_PAYMENT_METHOD", {
+    'AMAZON.YesIntent': function() {
+        this.emitWithState("ConfirmAccountAmount");
     },
     'ConfirmAccountAmount': function() {
-        currentState = "NONE";
+        this.handler.state = "NONE";
+        console.log("current state is " + this.handler.state);
         this.emit(":tell", "Okay, the  current account balance is 1500 CHF after this payment. Your medicine will be delivered to your door in about 20 minutes." + recheckMsg);
-        console.log("current state is " + currentState);
+    },
+    'AMAZON.NoIntent': function() {
+      this.handler.state = "NONE";
+      this.emit(":tell", "Fine. then keep 50 Francs handy. Your medicine will be delivered to your door in about 20 minutes." + recheckMsg);
+    },
+    'Unhandled': function() {
+      console.log("Can't understand user input " + this.handler.state);
+      this.emit(":ask", "Sorry Doora, I didn't understand you could you say that again");
+    }
+});
+
+var noneHandlers = Alexa.CreateStateHandler("_NONE", {
+  'OrderAspirinIntent': function() {
+    this.handler.state = "_CONFIRM_REQUEST_MEDICINE";
+    this.emit(":ask", "I have already ordered it twice this week. Are you sure that you want to order more and remove"
+    + " rum from your shopping list?");
+    console.log("current state is " + this.handler.state);
+  },
+  'Unhandled': function() {
+    console.log("Can't understand user input " + this.handler.state);
+    this.emit(":ask", "Sorry Doora, I didn't understand you could you say that again");
+  }
+});
+
+
+var handlers = {
+    'LaunchRequest': function () {
+      this.handler.state = "_NONE";
+      this.emit(":ask", 'Dora, <break time="5s"/> how are you and how was the tea party last night?');
+      console.log("current state is " + this.handler.state);
+    },
+    'Unhandled': function() {
+      console.log("Can't understand user input " + this.handler.state);
+      this.emit(":ask", "Sorry Doora, I didn't understand you could you say that again");
     }
 };
 
-
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context, callback);
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(confirmRequestMedicineHandlers, confirmMedicineHandlers, confirmPaymentMethods, noneHandlers, handlers);
     alexa.execute();
 };
